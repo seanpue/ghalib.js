fe2c4aaea4bb606d0b08b4c53c0139a758716727
A. Sean Pue <pue@msu.edu>
2015-08-03 12:25:52 -0400

    added ghalib.js generator

--- Cakefile
+++ Cakefile
@@ -1,6 +1,9 @@
 {exec} = require 'child_process'
 
-task 'build', "building dist directory", ->
+option '-b', '--base_url [BASE_URL]', 'set the base url for `ghalib.js`'
+task 'build', "building dist directory", (options)->
+  options.base_url or= 'http://www.columbia.edu/~asp49/00ghalib/'
+  console.log('Using base url of '+options.base_url)
   exec([
     "coffee --compile --bare --output lib/ src/"
     "mkdir -p ./dist"
@@ -8,6 +11,7 @@ task 'build', "building dist directory", ->
     "python generate_parser_data.py"
     "cp -pr ./lib ./dist/"
     "cp -pr ./css ./dist/"
+    "python generate_ghalib_js.py > dist/ghalib.js"
   ].join(' && '), (err, stdout, stderr) ->
     if err then console.log stderr.trim() else console.log 'done'
   )
---
+++ generate_ghalib_js.py
@@ -0,0 +1,26 @@
+import sys
+from string import Template
+
+import datetime
+
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d::%H:%M:%S")
+
+timestamp = "?"+timestamp
+
+if len(sys.argv) > 1:
+  base_url = sys.argv[1]
+else:
+  base_url = 'http://www.columbia.edu/~asp49/00ghalib/'
+
+assert base_url[-1]=='/'
+
+s = Template('''
+base_url = '$base_url';
+document.write('<link rel="stylesheet" type="text/css" href=base_url+"css/ghalib.js.css?v=$timestamp" />');
+var js = ["lib/urdu_parser_data.js","lib/devanagari_parser_data.js","lib/diacritics_parser_data.js","lib/ghalib.js","lib/jquery.min.js","lib/ghalib-loader.js"]
+for (var i = 0, l = js.length; i < l; i++) {
+    document.write("<script src=\"" + base_url + js[i] + "?v=$timestamp" + "\" type=\"text/javascript\"></scr" + "ipt>");
+}
+''')
+
+print s.substitute(base_url = 'http://www.columbia.edu/~asp49/00ghalib/', timestamp=timestamp)
