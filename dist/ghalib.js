9f7435a4a64ff5966f4cfb1365828e9f84b33688
A. Sean Pue <pue@msu.edu>
2016-05-12 15:57:17 -0400

    remove alert

--- dist/ghalib.js
+++ dist/ghalib.js
@@ -1,8 +1,8 @@
 
 base_url = 'http://www.columbia.edu/~asp49/00ghalib/';
-document.write('<link rel="stylesheet" type="text/css" href='+base_url+'css/ghalib.js.css?v=?2016-05-12::15:53:27" />');
+document.write('<link rel="stylesheet" type="text/css" href='+base_url+'css/ghalib.js.css?v=?2016-05-12::15:56:04" />');
 var js = ["lib/urdu_parser_data.js","lib/devanagari_parser_data.js","lib/diacritics_parser_data.js","lib/ghalib.js","lib/jquery.min.js","lib/jquery.colorbox-min.js","lib/ghalib-loader.js"]
 for (var i = 0, l = js.length; i < l; i++) {
-    document.write('<script src="' + base_url + js[i] + '?v=?2016-05-12::15:53:27" type="text/javascript"></scr' + 'ipt>');
+    document.write('<script src="' + base_url + js[i] + '?v=?2016-05-12::15:56:04" type="text/javascript"></scr' + 'ipt>');
 }
 
